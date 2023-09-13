e61d92516422a7749efaf875477e7b237fedaab3
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-13 15:10:54 +0530

    model changes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
+const workOutModel = require('./models/workoutModels')
 
 // GET all workouts
 router.get('/', (req, res) => {
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-  res.json({mssg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+  const {title, load, reps} = req.body
+  
+  try {
+    const workout = await workOutModel.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 })
 
 // DELETE a workout
